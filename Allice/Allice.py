02c9327203eb708ed35c21a201cf3c4f2447337b
Nagie Khant <nagie.tk@gmail.com>
2020-08-14 22:54:36 -0700

    swapping over to a different python library

--- Allice/Allice.py
+++ Allice/Allice.py
@@ -1,1 +1,15 @@
-print("hello")
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+alliceChatBot = ChatBot("Allice")
+
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot based on the english corpus
+trainer.train("chatterbot.corpus.english")
+
+# Get a response to an input statement
+chatbot.get_response("Hello, how are you today?")
+
+#if __name__ == '__main__':
