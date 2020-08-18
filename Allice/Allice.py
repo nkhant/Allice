a9a12c7de2e38300a29dd567e6914b0bdaa9b96a
Nagie Khant <nagie.tk@gmail.com>
2020-08-17 23:40:25 -0700

    Added direction of solution to potential problem

--- Allice/Allice.py
+++ Allice/Allice.py
@@ -4,14 +4,30 @@ from chatterbot.trainers import ChatterBotCorpusTrain
 import discord 
 from discord.ext import commands
 
+# This denotes when Allice will understand the person using a command of hers
+# Example You: >help
 bot = commands.Bot(command_prefix='>')
 
+# Give bot ability to read people's messages
+# Make it reply like a normal person
+# Constantly reads what people type and checks database
+# if there is a match reply
+# Do this through a function in the command_prefix
+# This is function is currently only the idea. it checks for a certain character
+# we maybe can change prefix to be entire message and use it as a search
+def readMessages(bot, message):
+    if message.guild is None:
+        return ''
+    else:
+        return'$'
+
 # All commands start with bot.command(). bot.client() for client related things
+# ctx = context
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
-# Removed on upload. Cant replace with own user's input
+# Removed on upload. Can replace with own user's input
 bot.run('token')
 
 
