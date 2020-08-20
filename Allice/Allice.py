eaab3c44b466655f3b8ec8b6c14ccf1372c0c76f
Nagie Khant <nagie.tk@gmail.com>
2020-08-19 23:31:39 -0700

    figured out how to let bot read and reply without a command prefix

--- Allice/Allice.py
+++ Allice/Allice.py
@@ -1,12 +1,15 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrain
+#from chatterbot import ChatBot
+#from chatterbot.trainers import ChatterBotCorpusTrain
 
 import discord 
 from discord.ext import commands
+from discord.ext.commands import Bot
 
 # This denotes when Allice will understand the person using a command of hers
 # Example You: >help
 bot = commands.Bot(command_prefix='>')
+
+
 
 # Give bot ability to read people's messages
 # Make it reply like a normal person
@@ -15,31 +18,40 @@ bot = commands.Bot(command_prefix='>')
 # Do this through a function in the command_prefix
 # This is function is currently only the idea. it checks for a certain character
 # we maybe can change prefix to be entire message and use it as a search
-def readMessages(bot, message):
-    if message.guild is None:
-        return ''
-    else:
-        return'$'
+#def readMessages(bot, message):
+#    if message.guild is None:
+#        return ''
+#    else:
+#        return'$'
 
 # All commands start with bot.command(). bot.client() for client related things
 # ctx = context
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+ 
+# Does not need prefix to read message. Bit will use this to reply to user messages
+# Dont forget need to have checks so it doesnt reply to itself
+@bot.event
+async def on_message(message):
+    if message.content.startswith("ping"):
+        await message.channel.send("Pong")
+
+    await bot.process_commands(message)
 
 # Removed on upload. Can replace with own user's input
-bot.run('token')
+bot.run('TOKEN')
 
+## Chat Bot Library - will uncomment later--------------------------
+#alliceChatBot = ChatBot("Allice")
 
-alliceChatBot = ChatBot("Allice")
+## Create a new trainer for the chatbot
+#trainer = ChatterBotCorpusTrainer(chatbot)
 
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(chatbot)
+## Train the chatbot based on the english corpus
+#trainer.train("chatterbot.corpus.english")
 
-# Train the chatbot based on the english corpus
-trainer.train("chatterbot.corpus.english")
+## Get a response to an input statement
+#chatbot.get_response("Hello, how are you today?")
 
-# Get a response to an input statement
-chatbot.get_response("Hello, how are you today?")
-
-#if __name__ == '__main__':
+##if __name__ == '__main__':
